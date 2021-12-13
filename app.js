b3c3b3acb4d8a915a93572a9799c9206a9957502
Mufaddil khan <mufaddil2003@gmail.com>
2021-12-13 14:18:08 +0500

    Work Completed

---
+++ app.js
@@ -0,0 +1,743 @@
+// CHAPTER NO =21-25
+
+// var firstName = prompt();
+// var lastName = prompt();
+// var firstName, lastName, fullName;
+// fullName = firstName + lastName;
+// console.log(fullName)
+
+
+
+
+// var a=prompt("ENTER THE FAVOURITE MOBILE PHONE ")
+// var b=[" SAMSUNG ","MOTOROLA ","IPHONE","NOKIA","OPPO","TECHNO SPARK","REDME"]
+// var c="MY FAVOURITE PHONE IS"
+// console.log(c+a)
+// console.log(b.length)
+
+// var a=("PAKISTANI")
+// var b=("STRING =")
+// var c=("INDEX OF N IS =")
+// console.log(b+a)
+
+// console.log(c+a.indexOf("N"))
+
+
+// var a=("HELLO WORlD")
+// var b=("STRING =")
+// var c=("INDEX OF N IS =")
+// console.log(b+a)
+
+// console.log(c+a.indexOf("l"))
+
+// var a=("PAKISTANI")
+// var b=("STRING =")
+// var c=("INDEX OF N IS =")
+// console.log(b+a)
+
+// console.log(c+a.indexOf("I"))
+
+// var firstName = prompt(),lastName = prompt();
+
+// var firstName, lastName, fullName;
+// fullName = firstName + lastName;
+// console.log(fullName)
+
+
+
+    // var a="472"
+    // console.log(a)
+    // console.log(+a)
+
+
+    // var a=prompt("ENTER ANY THING YOU WANT IN UPPER CASE")
+    // a=a.toUpperCase()
+    // console.log(a)
+
+    // var a=prompt("ENTER ANY THING YOU WANT IN UPPER CASE")
+    //  a=a.tototalcase()
+    // console.log(a)
+
+
+    // const num = 35.36;
+    // const without = num.toString().replace(".", "");
+    // console.log(without)
+
+
+// var arra = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var b=prompt()
+// if(b=arra[length]){
+//     alert("FOUND")
+// }else(b!=arra[length])(
+// alert("NOT FOUND")
+// )
+
+
+
+// var a="The"
+
+// console.log("2")
+
+
+// var str = 'University of Karachi';
+// var ar = str.split(''); // empty string separator
+// console.log( ar )
+
+
+
+
+    // var a="PAKISTAN"
+    // a=a.charAt(7)
+    // console.log(a)
+
+
+// CHAPTER NO =21-25 {COMPLETED}
+
+
+
+
+
+
+
+
+
+// CHAPTER NO =26-30
+
+
+
+// var a=prompt("ENTER POSITIVE INTEGER")
+// var ans=Math.round(a)
+// var answ=Math.ceil(a)
+// var answer=Math.floor(a)
+// console.log(a)
+// console.log(ans)
+// console.log(answ)
+
+// console.log(answer)
+
+
+// var a=prompt("ENTER negative INTEGER")
+// var ans=Math.round(a)
+// var answ=Math.ceil(a)
+// var answer=Math.floor(a)
+// console.log(a)
+// console.log(ans)
+// console.log(answ)
+
+// console.log(answer)
+// var a = Math.abs(-4);
+// console.log(a)
+
+// var diceRoll = Math.floor( Math.random() * 6 ) +1;
+// alert('You rolled a ' + diceRoll);
+
+
+// var head = 1;
+// var tail = 2;
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+// if(floor === 0){
+//     document.write("0 <br> Random Coin Value: Head")
+// } else if(floor === 1)
+// {
+//     document.write("1 <br> Random Coin Value: Tails")
+// }
+
+
+// var randomNum ;
+// randomNum = Math.ceil(Math.random() * 100);
+// document.write("Random number between 1 and 100: " + randomNum)
+
+// var a= prompt( "ENTER THE WEIGHT IN KGS")
+// console.log(a,'kg') 
+
+
+
+// const num = Math.ceil(Math.random() * 10);
+// console.log(num);
+//  const gnum = prompt('Guess the number between 1 and 10 inclusive');
+//  if (gnum == num)
+//    console.log('Matched');
+//   else
+//    console.log('Not matched, the number was '+gnum);
+// CHAPTER NO =26-30 {COMPLETED}
+
+// CHAPTER NO =31-34
+
+
+// var currentdate =new Date();
+// console.log(currentdate)
+
+// var currentDate = new Date();
+// currentDate.toString;
+// var b = currentDate.toString();
+// var c = b.slice(4,7);
+// if (c === "Jan"){
+//     document.write("Current month: January")
+// }else if(c === "Feb"){
+//     document.write("Current month: February")
+// }else if(c === "Mar"){
+//     document.write("Current month: March")
+// }else if(c === "Apr"){
+//     document.write("Current month: April")
+// }else if(c === "May"){
+//     document.write("Current month: May")
+// }else if(c === "Jun"){
+//     document.write("Current month: June")
+// }else if(c === "Jul"){
+//     document.write("Current month: July")
+// }else if(c === "Aug"){
+//     document.write("Current month: August")
+// }else if(c === "Sep"){
+//     document.write("Current month: September")
+// }else if(c === "Oct"){
+//     document.write("Current month: October")
+// }else if(c === "Nov"){
+//     document.write("Current month: November")
+// }else if(c === "Dec"){
+//     document.write("Current month: December")
+// }
+
+// var currentDay = new Date();
+// currentDay.toString;
+// var b = currentDay.toString();
+// var c = b.slice(0,3);
+// document.write("Today is " + c);
+
+
+
+// var currentDate = new Date();
+// currentDate.toString;
+// var b = currentDate.toString();
+// var c = b.slice(0,3);
+// if (c === "Sat"){
+//     document.write("It's Fun Day")
+// }else if(c === "Sun"){
+//     document.write("It's Fun Day")
+// }else if(c === "Mon"){
+//     document.write()
+// }else if(c === "Tue"){
+//     document.write()
+// }else if(c === "Wed"){
+//     document.write()
+// }else if(c === "Thr"){
+//     document.write()
+// }else if(c === "Fri"){
+//     document.write()
+// }
+
+
+// var todayDate = new Date();
+// var Todaymilli = todayDate.getTime();
+// var todayMin = todayDate.getMinutes();
+// var diff = Todaymilli - todayMin;
+// var diffMin = diff/(1000*60*60);
+// var accurateMin = Math.floor(diffMin);
+
+ 
+// document.write("Current Date: " + todayDate);
+// document.write("<br> Elapsed milliseconds since midnight, Jan 1, 1970: " + Todaymilli);
+// document.write("<br> Elapsed minutes since midnight, Jan 1, 1970: " + accurateMin)
+
+// var a = new Date();
+// var b = a.toString();
+// var c = b.slice(16,19);
+// if (c === "12:"){
+//     document.write("It's PM")
+// }else if(c === "13:"){
+//     document.write("It's PM")
+// }else if(c === "14:"){
+//     document.write("It's PM")
+// }else if(c === "15:"){
+//     document.write("It's PM")
+// }else if(c === "16:"){
+//     document.write("It's PM")
+// }else if(c === "17:"){
+//     document.write("It's PM")
+// }else if(c === "18:"){
+//     document.write("It's PM")
+// }else if(c === "19:"){
+//     document.write("It's PM")
+// }else if(c === "20:"){
+//     document.write("It's PM")
+// }else if(c === "21:"){
+//     document.write("It's PM")
+// }else if(c === "22:"){
+//     document.write("It's PM")
+// }else if(c === "23:"){
+//     document.write("It's PM")
+// }else if(c === "24:"){
+//     document.write("It's AM")
+// }else if(c === "01:"){
+//     document.write("It's AM")
+// }else if(c === "02:"){
+//     document.write("It's AM")
+// }else if(c === "03:"){
+//     document.write("It's AM")
+// }else if(c === "04:"){
+//     document.write("It's AM")
+// }else if(c === "05:"){
+//     document.write("It's AM")
+// }else if(c === "06:"){
+//     document.write("It's AM")
+// }else if(c === "07:"){
+//     document.write("It's AM")
+// }else if(c === "08:"){
+//     document.write("It's AM")
+// }else if(c === "09:"){
+//     document.write("It's AM")
+// }else if(c === "10:"){
+//     document.write("It's AM")
+// }else if(c === "11:"){
+//     document.write("It's AM")
+// }
+// console.log(c)
+
+
+
+// var d = new Date("July 21, 1983 01:15:00");
+// d.setDate(15);
+// console.log(d)
+
+
+
+// class Date
+// {
+//     constructor(d,m,y)
+//     {
+//         this.d = d;
+//             this.m = m;
+//             this.y = y;
+//     }
+// }
+ 
+// // To store number of days in
+//     // all months from January to Dec.
+// let monthDays=[31, 28, 31, 30, 31, 30,
+//                             31, 31, 30, 31, 30, 31];
+// // This function counts number of
+//     // leap years before the given date                           
+// function countLeapYears(d)
+// {
+//     let years = d.y;
+  
+//         // Check if the current year needs to be considered
+//         // for the count of leap years or not
+//         if (d.m <= 2)
+//         {
+//             years--;
+//         }
+  
+//         // An year is a leap year if it is a multiple of 4,
+//         // multiple of 400 and not a multiple of 100.
+//         return Math.floor(years / 4) - Math.floor(years / 100) +
+//         Math.floor(years / 400);
+// }
+ 
+// // This function returns number
+//     // of days between two given dates
+// function getDifference(dt1,dt2)
+// {
+// // COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dt1'
+  
+//         // initialize count using years and day
+//         let n1 = dt1.y * 365 + dt1.d;
+  
+//         // Add days for months in given date
+//         for (let i = 0; i < dt1.m - 1; i++)
+//         {
+//             n1 += monthDays[i];
+//         }
+  
+//         // Since every leap year is of 366 days,
+//         // Add a day for every leap year
+//         n1 += countLeapYears(dt1);
+  
+//         // SIMILARLY, COUNT TOTAL NUMBER OF DAYS BEFORE 'dt2'
+//         let n2 = dt2.y * 365 + dt2.d;
+//         for (let i = 0; i < dt2.m - 1; i++)
+//         {
+//             n2 += monthDays[i];
+//         }
+//         n2 += countLeapYears(dt2);
+  
+//         // return difference between two counts
+//         return (n2 - n1);
+// }
+ 
+//  // Driver code
+// let dt1 = new Date(1, 1,2015 );
+// let dt2 = new Date(1, 1, 2021);
+// document.write("Difference between two dates is " +
+//                             getDifference(dt1, dt2));
+ 
+
+// var a=prompt()
+// var b= 2021
+// var c=b-a
+// console.log(c)
+// CHAPTER NO =31-34 {COMPLETED}
+
+
+
+
+
+
+// CHAPTER NO =35-38
+// function date (){
+//     var current= new Date()
+//     console.log(current)
+// }
+// date ()
+
+// function name (){
+//     var a=prompt("ENTER FIRST NAME ")
+//     var b=prompt("ENTER SECOND NAME ")
+//     var c=("HELLO WORLD:")
+//    alert(c + a+b )
+// }
+// name()
+
+
+
+
+// function abc (a,b,c,d){
+//     var hello =a+b+c+d
+//     var total =hello*100
+//     console.log(total)
+// }
+// abc (20,20,20,50)
+
+
+// function sumOfSquares(num) {
+//     var i;
+
+//     for (i=0;i<=num;i++)  {
+//         var sum=0;
+//         var i = i*i;
+//         sum = i;
+// }
+//      document.write("The sum of squares for numbers up to and including " +num+ "is " +sum );
+// }
+// var num = parseInt(prompt("Enter a number:"));
+// sumOfSquares(num);
+
+
+// function factorial(n) {
+//     if (n == 0) return 1;
+//     return n * factorial(n - 1);
+//   }
+   
+//   // Driver Code
+//   let num = 6;
+//   document.write("Factorial of " + num + " is " + factorial(num));  
+
+
+// var length = prompt("Enter a whole number for the length of your rectangle.");
+//         var width = prompt("Enter a whole number for the width of your rectangle.");
+//         function area(length, width) {
+//             return length * width;
+//         }
+//         function perimeter(length, width) {
+//             return 2*( length + width);
+//         }
+        
+//         document.writeln('The area of your rectangle is ' + area(length, width));         
+//         document.writeln('The perimeter of your rectangle is ' + perimeter(length, width));
+
+
+// function check_Palindrome(str_entry){
+//     // Change the string into lower case and remove  all non-alphanumeric characters
+//        var cstr = str_entry.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+//         var ccount = 0;
+//     // Check whether the string is empty or not
+//         if(cstr==="") {
+//             console.log("Nothing found!");
+//             return false;
+//         }
+//     // Check if the length of the string is even or odd 
+//         if ((cstr.length) % 2 === 0) {
+//             ccount = (cstr.length) / 2;
+//         } else {
+//     // If the length of the string is 1 then it becomes a palindrome
+//             if (cstr.length === 1) {
+//                 console.log("Entry is a palindrome.");
+//                 return true;
+//             } else {
+//     // If the length of the string is odd ignore middle character
+//                 ccount = (cstr.length - 1) / 2;
+//             }
+//         }
+//     // Loop through to check the first character to the last character and then move next
+//         for (var x = 0; x < ccount; x++) {
+//     // Compare characters and drop them if they do not match 
+//             if (cstr[x] != cstr.slice(-1-x)[0]) {
+//                 console.log("Entry is not a palindrome.");
+//                 return false;
+//             }
+//         }
+//         console.log("The entry is a palindrome.");
+//         return true;
+//     }
+//     check_Palindrome('madam');
+//     check_Palindrome('nurses run');
+//     check_Palindrome('fox');
+    
+
+
+
+
+// function uppercase(str)
+// {
+//   var array1 = str.split(' ');
+//   var newarray1 = [];
+    
+//   for(var x = 0; x < array1.length; x++){
+//       newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+//   }
+//   return newarray1.join(' ');
+// }
+// console.log(uppercase(prompt()));
+
+
+
+// function find_longest_word(str)
+// {
+//   var array1 = str.match(/\w[a-z]{0,}/gi);
+//   var result = array1[0];
+
+//   for(var x = 1 ; x < array1.length ; x++)
+//   {
+//     if(result.length < array1[x].length)
+//     {
+//     result = array1[x];
+//     } 
+//   }
+//   return result;
+// }
+// console.log(find_longest_word('Web Development Tutorial'));
+
+
+// function char_count(str, letter) 
+// {
+//  var letter_Count = 0;
+//  for (var position = 0; position < str.length; position++) 
+//  {
+//     if (str.charAt(position) == letter) 
+//       {
+//       letter_Count += 1;
+//       }
+//   }
+//   return letter_Count;
+// }
+
+// console.log(char_count('w3resource.com', 'o'));
+
+
+
+
+
+
+
+
+// function circum(){
+// var radius = 3;
+// var circumference = Math.PI * 2*radius;
+// console.log("The circumference is " + circumference);
+// }
+// circum()
+// function aRea (){
+// var radius = 3;
+
+// var area = Math.PI * radius*radius;
+// console.log("The area is " + area);
+// }
+// aRea()
+
+// CHAPTER NO =35-38 {COMPLETED}
+
+
+
+
+
+
+
+  
+//       // year to be checked
+//       var year =  prompt( );
+//       var leap = false;
+  
+//       // if the year is divided by 4
+//       if (year % 4 == 0) {
+  
+//         // if the year is century
+//         if (year % 100 == 0) {
+  
+//           // if year is divided by 400
+//           // then it is a leap year
+//           if (year % 400 == 0)
+//             leap = true;
+//           else
+//             leap = false;
+//         }
+        
+//         // if the year is not century
+//         else
+//           leap = true;
+//       }
+      
+//       else
+//         leap = false;
+  
+//       if (leap)
+// console.log(year + " is a leap year.");
+//       else
+// console.log(year + " is not a leap year.");
+    
+
+// function say(message) {
+//     console.log(message);
+// }
+
+// let result = say('Hello');
+// console.log('Result:', result);
+
+
+
+// function remVowel(str)
+// {
+//     let al = [ 'a', 'e', 'i', 'o', 'u',
+//                'A', 'E', 'I', 'O', 'U' ];
+//     let result = "";
+     
+//     for(let i = 0; i < str.length; i++)
+//     {
+         
+//         if (!al.includes(str[i]))
+//         {
+//             result += str[i];
+//         }
+//     }
+//     return result;
+// }
+ 
+// // Driver code
+// let str = "GeeeksforGeeks - A Computer Science " +
+//           "Portal for Geeks";
+// document.write(remVowel(str));
+ 
+
+
+
+// function findOccurrences() {
+//     var str = "in the sentence";
+//     var res = str.match(/[aeiou]{2}/g);
+//     return res ? res.length : 0;
+//   }
+  
+//   var found = findOccurrences();
+  
+//   console.log(found);
+
+
+
+
+
+// var a= 1
+// var b=(a*1000)
+// console.log(b,'m')
+// var b=(a* 100000)
+// console.log(b,'cm')
+// var b=(a*1000000)
+// console.log(b,'mm')
+
+
+
+
+
+
+
+
+
+// var nl = getNewLine()
+
+// function getNewLine() {
+// 	var agent = navigator.userAgent
+
+// 	if (agent.indexOf("Win") >= 0)
+// 		return "\r\n"
+// 	else
+// 		if (agent.indexOf("Mac") >= 0)
+// 			return "\r"
+
+//  	return "\r"
+
+// }
+
+// {
+//     var count = 1, working_hours, over_time;
+
+
+//     while(count<=10)
+//     {
+//         prompt("Enter the working hours of employee no %d: ", count);
+//         console.log("%d", working_hours);
+//         if(working_hours>40)
+//         {
+//             over_time = working_hours - 40;
+//             over_time_pay = over_time * 12.00;
+//             prompt("Employee No %d overtime pay is %.2f\\n", count, over_time_pay);
+//         }
+//         else
+//             prompt("You have to work for more than 40 hours to get over time pay\\n");
+
+//         count++;
+//     }
+// }
+
+// document.write(pagecode);
+
+
+
+
+
+// var nl = getNewLine()
+
+// function getNewLine() {
+// 	var agent = navigator.userAgent
+
+// 	if (agent.indexOf("Win") >= 0)
+// 		return "\r\n"
+// 	else
+// 		if (agent.indexOf("Mac") >= 0)
+// 			return "\r"
+
+//  	return "\r"
+
+// }
+
+// pagecode = 'int main();'
+// {
+// var  amount;
+  
+//     // taking input   
+//     prompt("Enter the amount to be withdrawn (in hundreds): ");
+//     console.log("%d",amount);
+   
+//     //calculations
+//     prompt("\\n\\nRequired notes of Rs. 100  :  %d", amount / 100);
+//     // e.g. if amount entered is 987 then 987 / 100 will answer 9 (because amount is an intiger variable) 
+   
+//     prompt("\\n\\nRequired notes of Rs. 50   :  %d", (amount % 100) / 50);
+//     //e.g. 987 % 100 will answer 87 and then 87 / 50 will answer 1.
+   
+//     prompt("\\n\\nRequired notes of Rs. 10   :  %d", (((amount % 100) % 50) / 10));
+   
+//     prompt("\\n\\nAmount still remaining Rs. :  %d", (((amount % 100) % 50) % 10));
+   
+  
+// }
+
+
+// document.write(pagecode);
